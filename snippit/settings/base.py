687e1c0797e8fc8767256f6d3585a643230c7db1
Bahattin Cinic <bahattincinic@gmail.com>
2014-06-16 18:18:05 +0300

    apps are added to the settings

--- snippit/settings/base.py
+++ snippit/settings/base.py
@@ -42,7 +42,13 @@ DJANGO_APPS = (
 )
 
 THIRD_PARTY_APPS = ()
-LOCAL_APPS = ()
+
+LOCAL_APPS = (
+    'account',
+    'api',
+    'auth',
+    'snippet',
+)
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
