ae48a5c05357b9161e92a3615bdbd50cb62f8e90
Bahattin Cinic <bahattincinic@gmail.com>
2014-06-20 16:58:57 +0300

    create dummpy template

--- snippit/urls.py
+++ snippit/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = patterns('',
-
+    url(r'^$', TemplateView.as_view(template_name='layout.html'), name='layout')
 )
